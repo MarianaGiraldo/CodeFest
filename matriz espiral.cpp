32e09ced2f1926217ec2a38416b1d48a80538359
AlexDemon <91505127+AlejandroOrtizCor@users.noreply.github.com>
2023-11-04 10:56:05 -0500

    Add files via upload

--- matriz espiral.cpp
+++ matriz espiral.cpp
@@ -14,12 +14,14 @@ using namespace std;
 ll** espiral(ll n){
     ll repeticiones,i,j,k;
     ll** matriz = new ll*[n];
+    // rellenar matriz con ceros
     for (i=0; i<n; i++) {
         matriz[i] = new ll[n];
         for (j=0; j<n; j++) {
             matriz[i][j] = 0;
         }
     }
+    // establecer limites
     if (n%2!=0){
         repeticiones=floor(n/2);
     }
@@ -27,22 +29,27 @@ ll** espiral(ll n){
         repeticiones=floor(n/2)-1;
     }
     for (j=0;j<repeticiones+1;j++){
+        // esquina superior izquierda
         if (j==0){
             matriz[j][j]=1;
         }else{
             matriz[j][j]=matriz[j][j-1]+1;
         }
+        // fila arriba
         for (i=j+1;i<n-j;i++){
             matriz[j][i]=matriz[j][i-1]+1;
         }
+        // columna derecha
         for (i=j+1;i<n-j;i++){
             for (k=n-j-1;k<n-j;k++){
                 matriz[i][k]=matriz[i-1][k]+1;
             }
         }
+        // fila abajo
         for (i=0;i<n-2*j-1;i++){
             matriz[n-j-1][n-i-j-2]=matriz[n-j-1][n-i-j-1]+1;
         }
+        // columna izquierda
         for (i=0;i<n-2-2*j;i++){
             for (k=0;k<j+1;k++){
                 matriz[n-i-j-2][k]=matriz[n-i-k-1][k]+1;
@@ -57,6 +64,7 @@ int main(){
     ll n,i,j;
     cin >> n;
     ll** matriz=espiral(n);
+    // imprimir matriz
     for (i=0;i<n;i++){
         for(j=0;j<n;j++){
             cout << matriz[i][j] << " ";
